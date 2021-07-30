f58f02844210a808f15526b53ef197650ecf0022
Stas Minakov <supermina999@gmail.com>
2021-07-30 14:54:32 -0700

    Added YesWorkflow

---
+++ gen_workflow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -jar ~/bin/yesworkflow-0.2.1.2.jar graph main.py \
+ -c graph.layout=tb -c graph.edgelabels=show -c graph.portlayout=group -c graph.programlabel=both -c graph.datalabel=both -c graph.view=combined \
+ | dot -Tpng -o workflow.png
--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import pandas as pd
 import os
 import shutil
 
+# @begin data_cleaning_workflow
+# @in input_dish.csv @uri file:data/Dish.csv
+# @in input_menu.csv @uri file:data/Menu.csv
+# @in input_menu_item.csv @uri file:data/MenuItem.csv
+# @in input_menu_page.csv @uri file:data/MenuPage.csv
+# @out output_dish.csv @uri file:result/Dish.csv
+# @out output_menu.csv @uri file:result/Menu.csv
+# @out output_menu_item.csv @uri file:result/MenuItem.csv
+# @out output_menu_page.csv @uri file:result/MenuPage.csv
+
+
+# @begin sqlite_import
+# @in input_dish.csv
+# @in input_menu.csv
+# @in input_menu_item.csv
+# @in input_menu_page.csv
+# @out dish
+# @out menu
+# @out menu_item
+# @out menu_page
 def init_db(data_folder):
     con = sqlite3.connect(':memory:')
     pd.read_csv(data_folder + '/Dish.csv').to_sql('Dish', con)
@@ -10,6 +30,7 @@ def init_db(data_folder):
     pd.read_csv(data_folder + '/MenuItem.csv').to_sql('MenuItem', con)
     pd.read_csv(data_folder + '/MenuPage.csv').to_sql('MenuPage', con)
     return con
+# @end sqlite_import
 
 
 def export_table(table_name, file_name, con):
@@ -19,17 +40,32 @@ def export_table(table_name, file_name, con):
     df.to_csv(file_name, index=False)
 
 
+# @begin sqlite_export
+# @in dish_agg
+# @in menu
+# @in menu_item_agg
+# @in menu_page_v2
+# @out output_dish.csv @uri file:result/Dish.csv
+# @out output_menu.csv @uri file:result/Menu.csv
+# @out output_menu_item.csv @uri file:result/MenuItem.csv
+# @out output_menu_page.csv @uri file:result/MenuPage.csv
 def export_db(result_folder, con):
     if os.path.exists(result_folder):
         shutil.rmtree(result_folder)
     os.mkdir(result_folder)
+
     export_table('DishAgg', result_folder + '/Dish.csv', con)
     export_table('Menu', result_folder + '/Menu.csv', con)
     export_table('MenuItemAgg', result_folder + '/MenuItem.csv', con)
     export_table('MenuPage', result_folder + '/MenuPage.csv', con)
+# @end sqlite_export
 
 
 def cleanup_missing_references(cursor):
+    # @begin remove_invalid_menu_pages
+    # @in menu_page
+    # @in menu
+    # @out menu_page_v2
     cursor.execute('''
             DELETE FROM MenuPage WHERE id in (
                 SELECT MenuPage.id FROM MenuPage
@@ -37,6 +73,13 @@ def cleanup_missing_references(cursor):
                 WHERE Menu.id is null
             )
         ''')
+    # @end remove_invalid_menu_pages
+
+    # @begin remove_invalid_menu_items
+    # @in menu_item
+    # @in dish
+    # @in menu_page_v2
+    # @out menu_item_v2
     cursor.execute('''
                 DELETE FROM MenuItem WHERE id in (
                     SELECT MenuItem.id FROM MenuItem
@@ -51,9 +94,14 @@ def cleanup_missing_references(cursor):
                 WHERE Dish.id is null
             )
         ''')
+    # @end remove_invalid_menu_items
 
 
 def check_reference_icv(cursor):
+    # @begin check_menu_item_icv
+    # @in menu_item_v2
+    # @in dish
+    # @in menu_page_v2
     rows = cursor.execute('''
             SELECT MenuItem.id FROM MenuItem
             LEFT JOIN Dish on MenuItem.dish_id = Dish.id
@@ -66,15 +114,24 @@ def check_reference_icv(cursor):
                 WHERE MenuPage.id is null
             ''').fetchall()
     assert len(rows) == 0
+    # @end check_menu_item_icv
+
+    # @begin check_menu_page_icv
+    # @in menu_page_v2
+    # @in menu
     rows = cursor.execute('''
                 SELECT MenuPage.id FROM MenuPage
                 LEFT JOIN Menu on MenuPage.menu_id = Menu.id
                 WHERE Menu.id is null
             ''').fetchall()
     assert len(rows) == 0
+    # @end check_menu_page_icv
 
 
 def fix_missing_dish_dates(cursor):
+    # @begin fix_dish_missing_dates
+    # @in dish
+    # @out dish_v2
     cursor.execute('''
                 UPDATE Dish
                 SET first_appeared = null
@@ -85,17 +142,24 @@ def fix_missing_dish_dates(cursor):
                  SET last_appeared = null
                  WHERE last_appeared = 0
              ''')
+    # @end fix_dish_missing_dates
 
 
 def check_dish_dates_icv(cursor):
+    # @begin check_dish_dates_icv
+    # @in dish_v2
     rows = cursor.execute('''
                     SELECT * FROM Dish
                     WHERE first_appeared > last_appeared
                 ''').fetchall()
     assert len(rows) == 0
+    # @end check_dish_dates_icv
 
 
 def aggregate_dishes(cursor):
+    # @begin aggregate_dishes
+    # @in dish_v2
+    # @out dish_agg
     cursor.execute('''
             CREATE TABLE DishAgg AS
             SELECT
@@ -110,11 +174,23 @@ def aggregate_dishes(cursor):
             FROM Dish
             GROUP BY name
         ''')
+    # @end aggregate_dishes
+
+    # @begin create_dish_mapping
+    # @in dish_v2
+    # @in dish_agg
+    # @out dish_mapping
     cursor.execute('''
             CREATE TABLE DishMapping AS
             SELECT Dish.id AS id, DishAgg.id AS mid FROM Dish
             LEFT JOIN DishAgg on Dish.name = DishAgg.name
         ''')
+    # @end create_dish_mapping
+
+    # @begin aggregate_menu_items
+    # @in menu_item_v2
+    # @in dish_mapping
+    # @out menu_item_agg
     cursor.execute('''
             CREATE TABLE MenuItemAgg AS
             SELECT MenuItem.id as id, menu_page_id, price, high_price,
@@ -123,7 +199,9 @@ def aggregate_dishes(cursor):
             FROM MenuItem
             LEFT JOIN DishMapping on MenuItem.dish_id = DishMapping.id
         ''')
+    # @end aggregate_menu_items
 
+# @end data_cleaning_workflow
 
 if __name__ == '__main__':
     con = init_db('data')
