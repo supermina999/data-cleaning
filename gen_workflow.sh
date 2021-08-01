8601b30953d64dd8294f0d1206fe00e665850d65
Stas Minakov <supermina999@gmail.com>
2021-07-31 20:00:47 -0700

    More workflow files

--- gen_workflow.sh
+++ gen_workflow.sh
@@ -2,3 +2,7 @@
 java -jar ~/bin/yesworkflow-0.2.1.2.jar graph main.py \
  -c graph.layout=tb -c graph.edgelabels=show -c graph.portlayout=group -c graph.programlabel=both -c graph.datalabel=both -c graph.view=combined \
  | dot -Tpng -o workflow.png
+java -jar ~/bin/yesworkflow-0.2.1.2.jar graph main.py \
+ -c graph.layout=tb -c graph.edgelabels=show -c graph.portlayout=group -c graph.programlabel=both -c graph.datalabel=both -c graph.view=combined \
+ > workflow.gv
+java -jar ~/bin/yesworkflow-0.2.1.2.jar extract main.py -c extract.listfile > workflow.yw
